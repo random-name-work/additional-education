a8a0e005a600f919d161f0cda54411086272b21c
Rufus251 <vladimerrybalkin@gmail.com>
2023-10-05 13:05:07 +1000

    added roles module, controller, service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [UserModule, DatabaseModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
