95f348620d35a9146f963d3e2662c1335393f7b8
Rufus251 <vladimerrybalkin@gmail.com>
2023-10-06 23:45:03 +1000

    added jwt module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { RolesModule } from './roles/roles.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, RolesModule],
+  imports: [UserModule, DatabaseModule, RolesModule, JwtModule],
   controllers: [AppController],
   providers: [AppService],
 })
