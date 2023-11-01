5a1ece58f0aa02a30e98e087acb3b525d55bdf09
Rufus251 <vladimerrybalkin@gmail.com>
2023-11-02 01:19:11 +1000

    added ui buttons

---
+++ client/index/src/components/UI/button-width-full/white-button-full.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-btn
+      flat
+      class="text-none"
+      variant="outlined"
+      color="#3d8be4"
+      min-height="50px"
+      max-height="50px"
+      :href="href"
+      block
+    >
+      <slot></slot>
+    </v-btn>
+  </template>
+  
+  <script>
+  export default {
+    name:"WhiteButtonFull",
+    props:{
+      href: [String]
+    }
+  };
+  </script>
+  
+  <style scoped lang="scss"></style>
+  
