5bfb5f3d1c0503beb8875509aa5eb9690e2767e4
Rufus251 <vladimerrybalkin@gmail.com>
2023-11-01 23:50:04 +1000

    added UI folder

---
+++ client/index/src/components/UI/white-button.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn
+    flat
+    class="text-none"
+    variant="outlined"
+    color="#3d8be4"
+    min-height="60px"
+    max-height="60px"
+    min-width="180px"
+    max-width="180px"
+    block
+  >
+    <slot></slot>
+  </v-btn>
+</template>
+
+<style scoped lang="scss"></style>
