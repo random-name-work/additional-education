1fce923ea927bf95d2c7e4260054c7c0d0cf4e60
Rufus251 <vladimerrybalkin@gmail.com>
2023-11-04 19:16:00 +1000

    added white-border-button for UI

--- client/course_catalog/src/components/UI/index.js
+++ client/course_catalog/src/components/UI/index.js
@@ -1,5 +1,5 @@
 import WhiteButton225 from "./button-width-225px/white-button.vue"
-
+import WhiteBorderButton225 from "./button-width-225px/white-border-button.vue"
 import BlueButton180 from "./button-width-180px/blue-button.vue"
 import WhiteButton180 from "./button-width-180px/white-button.vue"
 
@@ -10,6 +10,7 @@ import BlueButtonFull from "./button-width-full/blue-button-full.vue"
 
 export default [
     WhiteButton225,
+    WhiteBorderButton225,
     BlueButton180,
     WhiteButton180,
     WhiteButton140,
---
+++ client/index/src/components/UI/button-width-225px/white-border-button.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-btn
+      flat
+      class="text-none"
+      variant="outlined"
+      min-height="60px"
+      max-height="60px"
+      min-width="225px"
+      max-width="225px"
+      :href="href"
+      block
+    >
+      <slot></slot>
+    </v-btn>
+  </template>
+  
+  <script>
+  export default {
+    name:"WhiteBorderButton225",
+    props:{
+      href: [String]
+    }
+  };
+  </script>
+  
+  <style scoped lang="scss"></style>
+  
