9e498c550110a0c928a3948e22724413543a1e9f
Rufus251 <vladimerrybalkin@gmail.com>
2023-11-05 00:02:39 +1000

    added checkbox ui

--- client/course_page/src/components/UI/index.js
+++ client/course_page/src/components/UI/index.js
@@ -1,5 +1,6 @@
 import WhiteButton225 from "./button-width-225px/white-button.vue"
 import WhiteBorderButton225 from "./button-width-225px/white-border-button.vue"
+
 import BlueButton180 from "./button-width-180px/blue-button.vue"
 import WhiteButton180 from "./button-width-180px/white-button.vue"
 
@@ -8,6 +9,7 @@ import WhiteButton140 from "./button-width-140px/white-button.vue"
 import WhiteButtonFull from "./button-width-full/white-button-full.vue"
 import BlueButtonFull from "./button-width-full/blue-button-full.vue"
 
+import CheckboxSwitcher from "./sort-checkbox/checkbox-switcher.vue"
 export default [
     WhiteButton225,
     WhiteBorderButton225,
@@ -15,5 +17,6 @@ export default [
     WhiteButton180,
     WhiteButton140,
     WhiteButtonFull,
-    BlueButtonFull
+    BlueButtonFull,
+    CheckboxSwitcher
 ]
